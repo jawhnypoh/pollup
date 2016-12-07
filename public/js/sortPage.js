347a55b4104c17691163360a17b71d6ce1025a0c
Daniel Lee <leed3@oregonstate.edu>
2016-12-06 19:25:51 -0800

    Add files via upload

--- public/js/sortPage.js
+++ public/js/sortPage.js
@@ -1,3 +1,21 @@
+function sortByNew(){
+  console.log("== clicked on new");
+  window.location.href = "sortPageNew.html";
+}
+
+function sortByTrending(){
+  console.log("== clicked on trending");
+  window.location.href = "sortPageTrending.html";
+}
+
+function sortByPopular(){
+  console.log("== clicked on popular");
+  window.location.href = "sortPagePopular.html";
+}
+
+// For the nav-bar dropdown menu
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -13,6 +31,23 @@ window.onclick = function(event) {
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
+    }
+  }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function genreSort() {
+    document.getElementById("genreMyDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.genreDropdown')) {
+
+    var dropdowns = document.getElementsByClassName("genre-dropdown-content");
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
 }
